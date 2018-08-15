b9e467fb7c629ba2496bb915e91188ca43427b3a
tqbdev <tranquocbao3897@gmail.com>
2018-08-15 23:11:45 +0700

    Add some packages to run project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 app = express()
-app.use(serveStatic(__dirname + "/dist"))
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
 var PORT = process.env.PORT || 3000
 app.listen(PORT)
